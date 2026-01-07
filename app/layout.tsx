960471cf00b18b37da3d92140d663c5adf91bdb5
Ravidu Rathnayaka <rmrhrathnayaka@gmail.com>
2026-01-07 18:06:53 +0530

    feat(ui): add navbar with dark mode functionalities

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/general/NavBar";
+import { ThemeProvider } from "@/components/general/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,12 +25,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NavBar />
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NavBar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/general/NavBar.tsx
+++ components/general/NavBar.tsx
@@ -1,10 +1,11 @@
 import { ChefHat } from "lucide-react";
 import Link from "next/link";
 import { buttonVariants } from "../ui/button";
+import { ThemeToggle } from "./ThemeToggle";
 
 const Navbar = async () => {
   return (
-    <nav className="flex justify-between items-center py-5">
+    <nav className="flex justify-between items-center py-5 px-5 border-b-2">
       <Link href={"/"}>
         <div className="flex gap-3 justify-center items-center">
           <ChefHat className="text-primary w-10 h-10" />
@@ -13,8 +14,9 @@ const Navbar = async () => {
       </Link>
       <div className="hidden md:flex items-center gap-5">
         <Link href="/create-recipe" className={buttonVariants({ size: "lg" })}>
-          Post Job
+          Create Recipe
         </Link>
+        <ThemeToggle />
         <Link
           href="/login"
           className={buttonVariants({ variant: "outline", size: "lg" })}
