de65c1944c740c5064f2e560eca52ea1138c34aa
Ravidu Rathnayaka <rmrhrathnayaka@gmail.com>
2026-01-07 17:39:01 +0530

    chore: initial project setup with NextJS

---
+++ components/general/NavBar.tsx
@@ -0,0 +1,29 @@
+import { ChefHat } from "lucide-react";
+import Link from "next/link";
+import { buttonVariants } from "../ui/button";
+
+const Navbar = async () => {
+  return (
+    <nav className="flex justify-between items-center py-5">
+      <Link href={"/"}>
+        <div className="flex gap-3 justify-center items-center">
+          <ChefHat className="text-primary w-10 h-10" />
+          <h1 className="text-primary text-2xl font-bold">MyRecipeBook</h1>
+        </div>
+      </Link>
+      <div className="hidden md:flex items-center gap-5">
+        <Link href="/create-recipe" className={buttonVariants({ size: "lg" })}>
+          Post Job
+        </Link>
+        <Link
+          href="/login"
+          className={buttonVariants({ variant: "outline", size: "lg" })}
+        >
+          Login
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
